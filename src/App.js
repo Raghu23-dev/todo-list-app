aee98d159281d866f42265952d9b87cc7622cfd0
raghu23-dev <raghu2308.dev@gmail.com>
2024-08-18 20:36:33 +0530

    final styling fixed all the glitches and added a style to header(h1 and h4) also added a page logo

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import DisplayTodos from "./components/DisplayTodos";
 function App() {
   return (
     <div className="App">
-      <h1>Todo List</h1>
+      <h1><img width="40px" src="https://cdn-icons-png.flaticon.com/128/4472/4472515.png" alt=""></img>Todo List</h1>
       <h4>Plan your day, then work your plan</h4>
       <Todos />
       <DisplayTodos />
