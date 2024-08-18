58f6f9ce59f25d6cf4f590ac3bf29c171ff5298d
raghu23-dev <raghu2308.dev@gmail.com>
2024-08-18 21:12:19 +0530

    added favicon for the site

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./css/main.css";
 import Todos from "./components/Todos";
 import DisplayTodos from "./components/DisplayTodos";
 
 function App() {
+  useEffect(() => {
+    document.title = "Todo List";
+  }, []);
+
   return (
+    
     <div className="App">
-      <h1><img width="40px" src="https://cdn-icons-png.flaticon.com/128/4472/4472515.png" alt=""></img>Todo List</h1>
+      <h1><img width="40px" src="https://cdn-icons-png.flaticon.com/128/4472/4472515.png" alt="" /> Todo List</h1>
       <h4>Plan your day, then work your plan</h4>
       <Todos />
       <DisplayTodos />
